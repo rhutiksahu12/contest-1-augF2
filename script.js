e1d244b08cc51ef57ebfdecf9c940ed3c43d07e3
rhutiksahu12 <shubhamsahu938@gmail.com>
2023-08-12 17:17:32 +0200

    finished

--- script.js
+++ script.js
@@ -4,23 +4,40 @@ let arr = [
   { id: 3, name: 'karen', age: 19, profession: 'admin' },
 ];
 
+let arr2 = [
+  { id: 5, name: 'Shreyas', age: 23, profession: 'developer' },
+  { id: 6, name: 'Rushab', age: 26, profession: 'Analyst' },
+  { id: 7, name: 'Ayushi', age: 24, profession: 'Developer' }
+]
 // Don't worry about consoling these functions, they are already being called on the button clicks.
 // Please don't change anything in the index.html file.
 
 function PrintDeveloper() {
   //Write your code here , just console.log
+  console.log(arr.filter((item) => item.profession === 'developer'));
+
 }
 
 function addData() {
   //Write your code here, just console.log
+  let employee = { id: 4, name: 'rhutik', age: 18, profession: 'developer' }
+  arr.push(employee);
+  console.log(arr)
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  const filteredarr = arr.filter(item =>
+    item.profession !== 'admin')
+
+  console.log(filteredarr)
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  arr= arr.concat(arr2)
+  // arr.push(...arr2)
+  console.log(arr)
 }
 
 // Here is an example of how functions work,
@@ -30,4 +47,5 @@ function concatenateArray() {
 function consoleArr() {
   console.log('Consoling Array Variable', arr);
   // Over here I can directly access the variable.
+
 }
